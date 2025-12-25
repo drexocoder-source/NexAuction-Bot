853b121d0b2b3e669326f027e8f66ff7d5f6f7c6
drexocoder-source <drexocoder@gmail.com>
2025-12-25 23:23:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 class Config:
     API_ID = 25887786
     API_HASH = "e4201277f5f2883f22c150167bd24479"
-    BOT_TOKEN = "8354037502:AAGx121BjLjekFjY_OmYMYq3PagNaFlnv70"
+    BOT_TOKEN = "8597478787:AAGJhcAxakpzOZW-Dwgghf7nkihLt0jNIEg"
     MONGO_URI = "mongodb+srv://wrik:wri13072005@cluster0.hnwak.mongodb.net/?retryWrites=true&w=majority"
     LOG_CHANNEL = -1003149414375
     WAITTIME = 30

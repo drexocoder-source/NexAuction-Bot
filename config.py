95faf4c724d43e70087ef8cbd4ee208d69686f86
drexocoder-source <drexocoder@gmail.com>
2025-12-30 16:44:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Config:
     API_HASH = "e4201277f5f2883f22c150167bd24479"
     BOT_TOKEN = "8597478787:AAGJhcAxakpzOZW-Dwgghf7nkihLt0jNIEg"
     MONGO_URI = "mongodb+srv://wrik:wri13072005@cluster0.hnwak.mongodb.net/?retryWrites=true&w=majority"
-    LOG_CHANNEL = -1003149414375
+    LOG_CHANNEL = -1003640029768
     WAITTIME = 30
